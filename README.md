d28fc1e4ad25ab6c360e7d743611ff1417de7e50
Amore Pietro <piero@ingamore.it>
2022-01-31 15:46:55 +0100

    Pubblicazione aggiornamento

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Web Services NodeJS Backend - miniservizi_old - v1.0.0
+# Web Services NodeJS Backend - miniservizi - v1.0.2
+- WebSite Statico con la directory __public__ (__pagehome.html__ e __pageabout.html__)
+- WebSite Dinamico (esempi __pagehome.ejs__ e __pageabout.ejs__)
+- Primo prototipo __pagine statiche__ con __Bootstrap v.5__
+---
+# Web Services NodeJS Backend - miniservizi - v1.0.0
--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express = require('express')
+const os = require("os");
+const express = require('express');
+
 const app = express()
 
-app.get('/', (req, res) => res.send('<h1>Homepage miniservizi_old</h1>') )
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+// ============ Contenuti dinamici ====================
+app.get('/', (req, res) => {
+    res.render('pages/pagehome', { hostname: os.hostname() })
+})
+
+app.get('/pageabout', (req, res) => {
+    res.render('pages/pageabout')
+})
+
+app.get('/about', (req, res) => res.send({ name: "Peter", age: 70 }) )
 
 app.listen(3000, () => console.log('Server is up on port 3000') )
 module.exports = app
